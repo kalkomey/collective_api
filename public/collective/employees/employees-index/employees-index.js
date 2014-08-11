b204f835a52f51c6bd4c6f572671128f75b4ecb5
R.A. Ray <rray@kalkomey.com>
2014-08-11 14:44:44 -0500

    added simple employee delete feature

--- public/collective/employees/employees-index/employees-index.js
+++ public/collective/employees/employees-index/employees-index.js
@@ -2,17 +2,18 @@ angular
   .module('Collective')
   .controller('EmployeesIndexController', function($scope, Employee){
 
-    console.log(Employee);
-
     Employee.getList().then(function(employees){
-      $scope.employees = employees
+      $scope.employees = employees;
     });
 
+    // add a new employee form
     $scope.add = function() {
       $scope.employee = {};
     };
 
+    // save the new employee
     $scope.save = function() {
+      // create a copy so we don't ...
       var model = angular.copy($scope.employee);
 
       Employee.post({employee: model});
@@ -22,7 +23,17 @@ angular
       delete $scope.employee;
     };
 
+    // remove the new employee form
     $scope.cancel = function() {
       delete $scope.employee;
+    };
+
+    // delete an employee completely
+    $scope.delete = function(employee) {
+      Employee.delete(employee);
+
+      var index = $scope.employees.indexOf(employee);
+
+      $scope.employees.splice(index, 1);
     };
   });
