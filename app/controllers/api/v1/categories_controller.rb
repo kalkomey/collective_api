40b491f945415fa55f41c5d82bf9c6e500e480ae
Joshua Kappers <josh@jkappers.me>
2014-08-11 17:37:59 -0500

    Added endpoints for category. Cleaned up unused views.

---
+++ app/controllers/api/v1/categories_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::GroupsController < ApplicationController
+  before_action :set_category, only: [:update, :destroy]
+
+  def index
+    @resources = Categories.all
+  end
+
+  def create
+    @resource = Category.new(category_params)
+
+    if @resource.save
+      render status: :created, nothing: true
+      return
+    end
+
+    render json: @resource.errors, status: :unprocessable_entity
+    return
+  end
+
+  def update
+    if @resource.update(category_params)
+      head :no_content
+    else
+      render json: @resource.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @resource.destroy
+    head :no_content
+  end
+
+  def categories
+    @categories = Group.uniq.pluck(:category)
+  end
+
+  private
+
+    def set_category
+      @resource = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :categories, except: [:new, :edit, :show]
       resources :employees
       resources :groups do
         collection do
