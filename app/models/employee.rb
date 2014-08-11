6cf7cec4449cfc9d01522af876e0366806b78731
Joshua Kappers <josh@jkappers.me>
2014-08-11 14:53:37 -0500

    Added dependent destroy on employee memberships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ActiveRecord::Base
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :groups, through: :memberships
   validates :name, presence: true, uniqueness: true
 end
