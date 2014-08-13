6cc220fd643dbfa5919490360ab6be9647c1c369
Joshua Kappers <josh@jkappers.me>
2014-08-13 16:49:01 -0500

    Updated Group api

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,57 +1,41 @@
 class Api::V1::GroupsController < ApplicationController
-  before_action :set_api_v1_group, only: [:edit, :update, :destroy]
+  before_action :set_group, only: [:edit, :update, :destroy]
 
   def index
-    @api_v1_groups = Group.all
+    @resources = Group.all
   end
 
   def show
     @resource = Group.includes(:category, memberships: :employee).find(params[:id])
   end
 
-  def new
-    @api_v1_group = Group.new
-  end
-
-  def edit
-  end
-
   def create
-    @api_v1_group = Group.new(api_v1_group_params)
+    @resource = Group.new(group_params)
 
-    respond_to do |format|
-      if @api_v1_group.save
-        format.html { redirect_to @api_v1_group, notice: 'Group was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @api_v1_group }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @api_v1_group.errors, status: :unprocessable_entity }
-      end
+    if @resource.save
+      render action: 'show', status: :created
+      return
     end
+
+    render json: @resource.errors, status: :unprocessable_entity
+    return
   end
 
   def update
-    respond_to do |format|
-      if @api_v1_group.update(api_v1_group_params)
-        format.html { redirect_to @api_v1_group, notice: 'Group was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @api_v1_group.errors, status: :unprocessable_entity }
-      end
+    if @resource.update(employee_params)
+      head :no_content
+    else
+      render json: @resource.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @api_v1_group.destroy
-    respond_to do |format|
-      format.html { redirect_to api_v1_groups_url }
-      format.json { head :no_content }
-    end
+    @resource.destroy
+    head :no_content
   end
 
   def search
-    @api_v1_groups =
+    @resources =
       Group.includes(:category)
            .where(categories: { name: params[:category] })
            .all
@@ -65,13 +49,12 @@ class Api::V1::GroupsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_api_v1_group
-      @api_v1_group = Group.find(params[:id])
+
+    def set_group
+      @resource = Group.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def api_v1_group_params
-      params[:api_v1_group]
+    def group_params
+      params.require(:group).permit(:name, :description, :category_id)
     end
 end
--- app/views/api/v1/groups/index.json.jbuilder
+++ app/views/api/v1/groups/index.json.jbuilder
@@ -1,4 +1,3 @@
-json.array!(@api_v1_groups) do |api_v1_group|
-  json.extract! api_v1_group, :id, :name, :category, :description
-  json.url api_v1_group_url(api_v1_group, format: :json)
+json.array!(@resources) do |resource|
+  json.extract! resource, :id, :name, :category, :description
 end
