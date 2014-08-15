ac5993bccbe8998ce5159ec8b5fac168ea73f87b
R.A. Ray <rray@kalkomey.com>
2014-08-15 12:39:55 -0500

    now checking for the existence of a person panel before adding it to the view

--- public/collective/people/people-index/people-index.js
+++ public/collective/people/people-index/people-index.js
@@ -2,18 +2,58 @@ angular
   .module('Collective')
   .controller('PeopleIndexController', function($scope, Person){
 
-    Person.getList().then(function(people){
-      $scope.people = people;
-      $scope.displayed = [];
-    });
+    Person
+      .getList()
+      .then(function(people) {
+
+        $scope.people = people;
+        $scope.displayed = [];
+      });
 
     // add a new person form
     $scope.add = function() {
+
       $scope.person = {};
+    };
+
+    // remove the new person form
+    $scope.cancel = function() {
+
+      delete $scope.person;
+    };
+
+    // add a person panel to the view
+    $scope.display = function(person) {
+
+      if ($scope.displayed.indexOf(person) !== -1) {
+
+        return;
+      }
+
+      $scope.displayed.push(person);
+    };
+
+    // hide a person panel from the view
+    $scope.hide = function(person) {
+
+      var index = $scope.displayed.indexOf(person);
+
+      $scope.displayed.splice(index, 1);
+    };
+
+    // delete an person completely
+    $scope.remove = function(person) {
+
+      person.remove();
+
+      var index = $scope.people.indexOf(person);
+
+      $scope.people.splice(index, 1);
     };
 
     // save the new person
     $scope.save = function() {
+
       // create a copy so we don't ... ?
       var model = angular.copy($scope.person);
 
@@ -22,31 +62,5 @@ angular
       $scope.people.push(model);
 
       delete $scope.person;
-    };
-
-    // remove the new person form
-    $scope.cancel = function() {
-      delete $scope.person;
-    };
-
-    // hide a person panel from the view
-    $scope.hide = function(person) {
-      var index = $scope.displayed.indexOf(person);
-
-      $scope.displayed.splice(index, 1);
-    };
-
-    // delete an person completely
-    $scope.remove = function(person) {
-      person.remove();
-
-      var index = $scope.people.indexOf(person);
-
-      $scope.people.splice(index, 1);
-    };
-
-    // add a person panel to the view
-    $scope.display = function(person) {
-      $scope.displayed.push(person);
     };
   });
