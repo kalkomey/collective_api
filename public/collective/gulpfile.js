c1281fd0789b0ce42c1bd0f231992c003dcc8cc0
Ryan Rushing <rrushing@kalkomey.com>
2014-08-18 15:53:36 -0500

    moved the app.less into a styles directory for better long term organization

--- public/collective/gulpfile.js
+++ public/collective/gulpfile.js
@@ -36,7 +36,7 @@ gulp.task('clean', function() {
 });
 
 gulp.task('css', ['clean'], function() {
-    return gulp.src('app.less')
+    return gulp.src('styles/app.less')
         .pipe(less())
         .pipe(cssmin({keepSpecialComments: 0}))
         .pipe(rename('app.full.min.css'))
