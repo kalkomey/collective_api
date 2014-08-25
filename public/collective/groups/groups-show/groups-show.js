b610b397ee2b1faa03ee59034cf27adbb4af934b
R.A. Ray <rray@kalkomey.com>
2014-08-25 08:58:42 -0500

    groups can now be added to people

--- public/collective/groups/groups-show/groups-show.js
+++ public/collective/groups/groups-show/groups-show.js
@@ -2,6 +2,20 @@ angular
   .module('Collective')
     .controller('GroupsShowController', function($scope, PeopleContext, Group, Person, Membership) {
 
+      $scope.$watchCollection('group.people', function(newPeople, oldPeople) {
+
+        // we need to do something only if a person was added
+        if (newPeople.length <= oldPeople.length) {
+
+          return;
+        }
+
+        // for now we can assume that the last person is the new person
+        var newPerson = newPeople[newPeople.length - 1];
+
+        $scope.addPerson(newPerson);
+      });
+
       $scope.selectPerson = function(person) {
 
         var selPerson           = _.findWhere(PeopleContext.people, {id: person.id});
@@ -18,18 +32,4 @@ angular
 
         var promise = Membership.post({group_id: $scope.group.id, employee_id: person.id});
       };
-
-      $scope.$watchCollection('group.people', function(newPeople, oldPeople) {
-
-        // we need to do something only if a person was added
-        if (newPeople.length <= oldPeople.length) {
-
-          return;
-        }
-
-        // for now we can assume that the last person is the new person
-        var newPerson = newPeople[newPeople.length - 1];
-
-        $scope.addPerson(newPerson);
-      });
     });
--- public/collective/people/people-show/people-show.js
+++ public/collective/people/people-show/people-show.js
@@ -1,9 +1,23 @@
 angular
   .module('Collective')
     .controller('PeopleShowController',
-      function($scope, GroupsContext, Person) {
+      function($scope, GroupsContext, Person, Membership) {
 
-        $scope.groups       = $scope.person.categorizedGroups();
+        $scope.groups = $scope.person.categorizedGroups();
+
+        $scope.$watchCollection('person.groups', function(newGroups, oldGroups) {
+
+          // we need to do something only if a person was added
+          if (newGroups.length <= oldGroups.length) {
+
+            return;
+          }
+
+          // for now we can assume that the last person is the new person
+          var newGroup = newGroups[newGroups.length - 1];
+
+          $scope.addGroup(newGroup);
+        });
 
         $scope.selectGroup  = function(group) {
 
@@ -15,5 +29,10 @@ angular
 
           var selGroup          = _.findWhere(GroupsContext.groups, {id: group.id});
               selGroup.selected = false;
+        };
+
+        $scope.addGroup = function(group) {
+
+          var promise = Membership.post({group_id: group.id, employee_id: $scope.person.id});
         };
       });
